0a86f5e67ba20b4f0d2648471adf9bb2166b29ac
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-05 16:55:20 +0530

    [Tanvi] Add : UC1-Employee Attendance

---
+++ EmpWageProblem/EmpCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProblem
+{
+    public class EmpCheck
+    {
+        public static void Attendance()
+        {
+            Random random = new Random();
+            int empcheck = random.Next(0, 2);
+            if (empcheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ EmpWageProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpCheck empcheck = new EmpCheck();
+            EmpCheck.Attendance();
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
