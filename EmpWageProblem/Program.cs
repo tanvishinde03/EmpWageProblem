b609814eda0970e114fb785a2bf3ecb032dd0117
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-14 17:12:02 +0530

    [Tanvi] Add : Monthly Wage Of Employee

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -6,15 +6,17 @@ using System.Threading.Tasks;
 
 namespace EmpWageProblem
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             EmpCheck empcheck = new EmpCheck();
             EmpCheck.Attendance();
-            
+            Console.ReadLine();
+
             DailyWage empcheckk = new DailyWage();
             DailyWage.EmpWage();
+            Console.ReadLine();
 
             PartTime empcheckkk = new PartTime();
             PartTime.EmpWagePart();
@@ -22,6 +24,13 @@ namespace EmpWageProblem
 
             Switchcase emppcheck = new Switchcase();
             Switchcase.SwitchCase();
+            Console.ReadLine();
+
+            MonthlyWage obj = new MonthlyWage();
+            MonthlyWage.EmployeeMonthlyWage();
+            Console.ReadLine();
+
+
         }
     }
 }
