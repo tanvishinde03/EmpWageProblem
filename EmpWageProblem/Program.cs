8b7d9c7ae8ccdbe94b95a1dad0b29b59ba99b0c7
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-14 19:46:42 +0530

    [Tanvi] Add : UC7 - Refector the code using class method

--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -6,35 +6,46 @@ using System.Threading.Tasks;
 
 namespace EmpWageProblem
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            EmpCheck empcheck = new EmpCheck();
-            EmpCheck.Attendance();
+
+        EmpCheck empcheck = new EmpCheck();
+        EmpCheck.Attendance();
+                Console.ReadLine();
+
+                DailyWage empcheckk = new DailyWage();
+        DailyWage.EmpWage();
+                Console.ReadLine();
+
+                PartTime empcheckkk = new PartTime();
+        PartTime.EmpWagePart();
+                Console.ReadLine();
+
+                Switchcase emppcheck = new Switchcase();
+        Switchcase.SwitchCase();
+                Console.ReadLine();
+
+                MonthlyWage obj = new MonthlyWage();
+        MonthlyWage.EmployeeMonthlyWage();
+                Console.ReadLine();
+
+                HoursDaysTotal Obj = new HoursDaysTotal();
+        HoursDaysTotal.DaysHoursOfMonth();
+                Console.ReadLine();
+
+            Refactor obJ = new Refactor();
+            Refactor.ComputeEmpWage();
             Console.ReadLine();
-
-            DailyWage empcheckk = new DailyWage();
-            DailyWage.EmpWage();
-            Console.ReadLine();
-
-            PartTime empcheckkk = new PartTime();
-            PartTime.EmpWagePart();
-            Console.ReadLine();
-
-            Switchcase emppcheck = new Switchcase();
-            Switchcase.SwitchCase();
-            Console.ReadLine();
-
-            MonthlyWage obj = new MonthlyWage();
-            MonthlyWage.EmployeeMonthlyWage();
-            Console.ReadLine();
-
-            HoursDaysTotal Obj = new HoursDaysTotal();
-            HoursDaysTotal.DaysHoursOfMonth();
-            Console.ReadLine();
-
-
-        }
+    }
 }
 }
+    
+
+
+
+
+    
+
+
