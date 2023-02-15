3fb1c48937f27258daac9a254cc3fd40b347df18
tanvishinde03 <shindetanvi0203@gmail.com>
2023-02-15 20:00:06 +0530

    [Tanvi] Add : UC7 - For Multiple Companies

---
+++ EmpWageProblem/MultipleCompanies.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageProblem
+{
+    public class MultipleCompanies
+    {
+         public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public static void ComputeWage(string Company, int empRateperhour, int numOfWorkingDays, int maxHrsInMonths)
+        {
+            
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            
+            while (totalEmpHrs <= maxHrsInMonths && totalWorkingDays < numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRateperhour;
+            Console.WriteLine("Total Emp Wage of {0}: {1} ", Company, totalEmpWage);
+          
+        }
+       
+    }
+}
+
+
+
+    
+
--- EmpWageProblem/Program.cs
+++ EmpWageProblem/Program.cs
@@ -11,35 +11,46 @@ namespace EmpWageProblem
         static void Main(string[] args)
         {
 
-        EmpCheck empcheck = new EmpCheck();
-        EmpCheck.Attendance();
-                Console.ReadLine();
 
-                DailyWage empcheckk = new DailyWage();
-        DailyWage.EmpWage();
-                Console.ReadLine();
+            EmpCheck empcheck = new EmpCheck();
+            EmpCheck.Attendance();
+            Console.ReadLine();
 
-                PartTime empcheckkk = new PartTime();
-        PartTime.EmpWagePart();
-                Console.ReadLine();
+            DailyWage empcheckk = new DailyWage();
+            DailyWage.EmpWage();
+            Console.ReadLine();
 
-                Switchcase emppcheck = new Switchcase();
-        Switchcase.SwitchCase();
-                Console.ReadLine();
+            PartTime empcheckkk = new PartTime();
+            PartTime.EmpWagePart();
+            Console.ReadLine();
 
-                MonthlyWage obj = new MonthlyWage();
-        MonthlyWage.EmployeeMonthlyWage();
-                Console.ReadLine();
+            Switchcase emppcheck = new Switchcase();
+            Switchcase.SwitchCase();
+            Console.ReadLine();
 
-                HoursDaysTotal Obj = new HoursDaysTotal();
-        HoursDaysTotal.DaysHoursOfMonth();
-                Console.ReadLine();
+            MonthlyWage obj = new MonthlyWage();
+            MonthlyWage.EmployeeMonthlyWage();
+            Console.ReadLine();
+
+            HoursDaysTotal Obj = new HoursDaysTotal();
+            HoursDaysTotal.DaysHoursOfMonth();
+            Console.ReadLine();
 
             Refactor obJ = new Refactor();
             Refactor.ComputeEmpWage();
             Console.ReadLine();
+
+            MultipleCompanies companies= new MultipleCompanies();      //UC8
+            MultipleCompanies.ComputeWage("HM", 30, 10, 70);
+            MultipleCompanies.ComputeWage("ZARA", 40, 20, 60);
+            Console.ReadLine();
+
+
+
+        }
+
+
     }
-}
 }
     
 
